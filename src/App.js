2842f98045097c8115da1edfbf486f7c25ce634a
crosscay <caicedocarlos0306@gmail.com>
2020-08-09 17:14:12 -0500

    subiendo primeros archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  // definir el state
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos ] = useState([]);
+
+  // Cuando agreguemos un nuevo gasto
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos,
+      gasto
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          { mostrarpregunta ? 
+          (
+          <Pregunta 
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+            actualizarPregunta={actualizarPregunta}
+          />
+          ) :
+          (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario 
+                  agregarNuevoGasto={agregarNuevoGasto}
+                />
+              </div>
+              <div className="one-half column">
+                2
+              </div>
+            </div>
+          )
+          }
+        </div>
       </header>
     </div>
   );
