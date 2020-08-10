2a3e12fdb4293be68d98b52eb199669c4a1ce51e
crosscay <caicedocarlos0306@gmail.com>
2020-08-09 19:30:20 -0500

    Updates

---
+++ precache-manifest.6561017cc5eb44096e5da4a9c70b1b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61f7bc606f36457f0cadaa1aed52093",
+    "url": "/control_de_presupuesto_react/index.html"
+  },
+  {
+    "revision": "70cd0e5d3b6afaa0ca47",
+    "url": "/control_de_presupuesto_react/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "511277825761959de1e3",
+    "url": "/control_de_presupuesto_react/static/js/2.cb3e2a0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/control_de_presupuesto_react/static/js/2.cb3e2a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70cd0e5d3b6afaa0ca47",
+    "url": "/control_de_presupuesto_react/static/js/main.ddc79186.chunk.js"
+  },
+  {
+    "revision": "0e2fb9968717936a5c3c",
+    "url": "/control_de_presupuesto_react/static/js/runtime-main.7f43e722.js"
+  }
+]);
