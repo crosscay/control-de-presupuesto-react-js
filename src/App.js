dfc77d33d4f9e3edfef43a941e5d345e5f2427a2
crosscay <caicedocarlos0306@gmail.com>
2020-08-09 19:09:57 -0500

    actualizando funcionalidad y documentando propTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
 
 function App() {
 
@@ -9,14 +11,29 @@ function App() {
   const [ restante, guardarRestante ] = useState(0);
   const [ mostrarpregunta, actualizarPregunta ] = useState(true);
   const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState([]);
+  const [ crearGasto, guardarCrearGasto ] = useState(false);
+
+  // UseEffect que actualiza el restante
+  useEffect(() => {
+    if(crearGasto) {
+
+      // agrega el nuevo presupuesto
+      guardarGastos([
+        ...gastos,
+        gasto
+      ])
+
+      // resta del presupuesto actual
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+
+      // resetar a false
+      guardarCrearGasto(false);
+    }
+  }, [gasto, crearGasto, gastos, restante]);
 
   // Cuando agreguemos un nuevo gasto
-  const agregarNuevoGasto = gasto => {
-    guardarGastos([
-      ...gastos,
-      gasto
-    ]);
-  }
 
   return (
     <div className="container">
@@ -36,11 +53,19 @@ function App() {
             <div className="row">
               <div className="one-half column">
                 <Formulario 
-                  agregarNuevoGasto={agregarNuevoGasto}
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
                 />
               </div>
               <div className="one-half column">
-                2
+                <Listado 
+                  gastos={gastos}
+                />
+
+                <ControlPresupuesto 
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
               </div>
             </div>
           )
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import Error from './Error';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import Error from './Error';
 
-const Formulario = ({agregarNuevoGasto}) => {
+const Formulario = ({guardarGasto, guardarCrearGasto}) => {
 
     const [ nombre, guardarNombre ] = useState('');
     const [ cantidad, guardarCantidad ] = useState(0);
@@ -28,7 +29,8 @@ const Formulario = ({agregarNuevoGasto}) => {
         }
 
         // pasar el gasto al componente principal
-        agregarNuevoGasto(gasto);
+        guardarGasto(gasto);
+        guardarCrearGasto(true);
 
         // resetear el form
         guardarNombre('');
@@ -71,6 +73,11 @@ const Formulario = ({agregarNuevoGasto}) => {
             />
         </form>
      );
+}
+
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired
 }
  
 export default Formulario;
